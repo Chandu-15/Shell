d446f2f74689e70c8e68189a88bd8619b4271e45
Chandana <rebbachandana54@gmail.com>
2025-11-27 12:43:20 +0000

    s

---
+++ installation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+USER_ID=$(id -u)
+
+#check whether script is running under root access or not
+if [ $USER_ID -ne 0 ]; then
+   echo "ERROR:Please execute the script under root access"
+   exit 1 # we are manually forcing to exit from execution if any error occured
+fi
+
+dnf install mysql -y
+if [ $? -ne 0 ]; then
+   echo "ERROR: Instally MYSQL is FAILURE"
+   exit 1
+else
+   echo "Installing MYSQL is SUCCESS"
