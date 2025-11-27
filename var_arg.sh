51b75aabade9aee10a6c939557cc3918e5d7e496
Chandana <rebbachandana54@gmail.com>
2025-11-27 10:48:22 +0000

    s

---
+++ var_arg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+name1=$1
+name2=$2
+echo "$name1: Hi, whats your plan for long weekend $name2 ?"
+echo "$name2: I am going for kodaikanal to enjoy a lot $name1."
+echo "$name1: Oh, thats great ...enjoy your trip"
+echo "$name2 : thank you so much "
