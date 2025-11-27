96a9adfcaf65620bf0bcbb88dd1ce178f8114899
Chandana <rebbachandana54@gmail.com>
2025-11-27 11:43:05 +0000

    s

--- var_dyn_2.sh
+++ var_dyn_2.sh
@@ -3,4 +3,4 @@ start_time=$(date +%s)
 sleep 10
 end_time=$(date +%s)
 total_time=$(($end_time - $start_time))
-echo "Script executed in total_time seconds"
+echo "Script executed in $total_time seconds"
