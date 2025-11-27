98cadda260d3934fae26cf33edee5d45884c5c5a
Chandana <rebbachandana54@gmail.com>
2025-11-27 13:14:56 +0000

    s

--- colors.sh
+++ colors.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 USER_ID=$(id -u)
+# giving colours as  per our wish 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -7,15 +8,15 @@ W="\e[0m"
 
 #check whether script is running under root access or not
 if [ $USER_ID -ne 0 ]; then
-   echo " $R ERROR:Please execute the script under root access $N"
+   echo -e " $R ERROR:Please execute the script under root access $N"
    exit 1 # we are manually forcing to exit from execution if any error occured
 fi
 #creating function for  validation once package installed 
 VALIDATE(){ if [ $1 -ne 0 ]; then
-   echo "ERROR....$R $2 is FAILURE $N"
+   echo -e "ERROR....$R $2 is FAILURE $N"
    exit 1
 else
-   echo "$R $2 is SUCCESS $N"
+   echo -e "$R $2 is SUCCESS $N"
 fi
 }
 # check whether package is installed or not if installed please ignore else install the package
@@ -24,7 +25,7 @@ if [ $? -ne 0 ]; then
   dnf install mysql -y
   VALIDATE $? "installing mysql"
 else
-  echo "Mysql already exists....$Y Skipping $N"
+  echo -e "Mysql already exists....$Y Skipping $N"
 fi
 
 dnf list installed nginx
@@ -32,7 +33,7 @@ if [ $? -ne 0 ]; then
  dnf install nginx -y
  VALIDATE $? "nginx"
 else
-  echo "Nginx already exists....$Y Skipping $N"
+  echo -e "Nginx already exists....$Y Skipping $N"
 fi
 
 dnf list installed python3
@@ -40,6 +41,6 @@ if [ $? -ne 0 ]; then
  dnf install python3 -y
  VALIDATE $? "python3"
 else
-  echo "Python3 already exists....$Y Skipping $N"
+  echo  -e "Python3 already exists....$Y Skipping $N"
 fi
 
