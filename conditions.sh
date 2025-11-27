bcfba64008717636c065740832a1730514dd1140
Chandana <rebbachandana54@gmail.com>
2025-11-27 12:18:10 +0000

    s

---
+++ conditions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+num =$1
+if [ $num -lt 10 ]; then
+ echo "Given number $num is less than 10"
+elif [ $num -gt 10 ]; then
+ echo "Given number $num is greater than 10"
+else
+ echo "Given number is equal to 10"
+ 
