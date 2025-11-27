c677960de1b5c9f18b950cd4d0f836a9de57922c
Chandana <rebbachandana54@gmail.com>
2025-11-27 12:13:25 +0000

    s

--- array.sh
+++ array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-leaders=("modi","gandi","nehru")
+leaders=("modi" "gandi" "nehru")
 echo "all leader is ${leaders[@]}"
 echo "first leader is ${leaders[0]}"
 echo "last leader is ${leaders[10]}"
