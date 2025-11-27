2e22f880ac6afcd8b8923b948dab5c027d1d3a1a
Chandana <rebbachandana54@gmail.com>
2025-11-27 11:00:17 +0000

    s

--- var_spl.sh
+++ var_spl.sh
@@ -1,7 +1,7 @@
 echo "please allow all variables to the script : $@"
 echo "please allow all variables to the script : $*"
 echo "script_name: $0"
-echo "current working directory : $pwd"
+echo "current working directory : $PWD"
 echo "Home directory : $HOME"
 echo "PID for this script: $$"
 sleep 40&
