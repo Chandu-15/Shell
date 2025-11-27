a3375eda81acecddd86776c7c399cfee2bc764a5
Chandana <rebbachandana54@gmail.com>
2025-11-27 10:44:27 +0000

    declare variables

--- var_1.sh
+++ var_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-nam1=ram
+name1=ram
 name2=sam
 echo "$name1: Hi, whats your plan for long weekend $name2 ?"
-echo "$name2: I am going for kodaikanal to enjoy a lot $name."
+echo "$name2: I am going for kodaikanal to enjoy a lot $name1."
 echo "$name1: Oh, thats great ...enjoy your trip"
 echo "$name2 : thank you so much "
